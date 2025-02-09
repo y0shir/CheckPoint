89b04724999036ff6835db0681f972b4c47b3250
fedor tvorogov <fedor_tvorogov@mail.ru>
2025-02-09 12:24:26 +0300

    crt BookC CarC CusC RootVC

---
+++ src/main/java/RootVegetableComporator.java
@@ -0,0 +1,13 @@
+//public class RootVegetableComporator implements CustomComparator<RootVegetableComporator>{
+//	@Override
+//	public int compare(RootVegetableComporator rootVgtbl1, RootVegetableComporator rootVgtbl2) {
+//		if (rootVgtbl1 != rootVgtbl2) {
+//			return CustomComparator.compareINT(rootVgtbl1, rootVgtbl2);
+//		} else if (!rootVgtbl1.equals(rootVgtbl2)) {
+//			return rootVgtbl1.compareTo(rootVgtbl2);
+//		} else {
+//			return CustomComparator.compareINT(rootVgtbl1, rootVgtbl2);
+//		}
+//	};
+//	}
+//}
